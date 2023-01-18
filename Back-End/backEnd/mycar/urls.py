813eaba7c6c0fdf5b9e11e9126f576a5fc60b975
jmamine <109294107+jmamine@users.noreply.github.com>
2023-01-18 13:15:45 +0100

    .

--- Back-End/backEnd/mycar/urls.py
+++ Back-End/backEnd/mycar/urls.py
@@ -8,7 +8,11 @@ urlpatterns =[
 path('login/',views.Login,name='login'),
 path('logout/',views.logout,name='logout'),
 path( 'all_objects/',views.all_objects,name='all_objects'),
-path( 'Registration/',views.Registration,name='Registration')
+path( 'Registration/',views.Registration,name='Registration'),
+path('filter/',views.filter,name='filter'),
+path('resrvation/',views.Reservation,name='reservation'),
+path('EndReservation',views.End_Reservation,name='endreservation')
+
 
 
 ]
