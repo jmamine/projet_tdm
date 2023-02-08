9e85dc3e398fcedb62e7d38a6546ffcd3e219bf5
Shoyoo01 <ja_diaf@esi.dz>
2023-02-08 15:20:06 +0100

    test

--- Front-End/Rental/app/src/main/java/com/example/rental/HomeFragment.kt
+++ Front-End/Rental/app/src/main/java/com/example/rental/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -35,6 +36,8 @@ class HomeFragment : Fragment() {
         val layoutManagerCars = GridLayoutManager(requireContext(), 2)
         viewCars.layoutManager = layoutManagerCars
         viewCars.adapter = CarAdapter(requireContext(), vmCars.data)
+        val hh = requireActivity().findViewById<TextView>(R.id.hh)
+
 
     }
 
