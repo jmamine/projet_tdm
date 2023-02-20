0193370e17d2c11b515453ea38c14469f7b9e76c
jmamine <109294107+jmamine@users.noreply.github.com>
2023-02-20 14:24:48 +0100

    .

--- Front-End/Rental/app/src/main/java/com/example/rental/HomeFragment.kt
+++ Front-End/Rental/app/src/main/java/com/example/rental/HomeFragment.kt
@@ -19,7 +19,10 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.*
 import sendGetRequest
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of df4633c (test1)
 
 
 class HomeFragment : Fragment() {
@@ -48,6 +51,7 @@ class HomeFragment : Fragment() {
         val layoutManagerCars = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
         viewCars.layoutManager = layoutManagerCars
         viewCars.adapter = CarAdapter(requireContext(), vmCars.data)
+<<<<<<< HEAD
 <<<<<<< HEAD
         val brand = requireActivity().findViewById<TextView>(R.id.brand)
 
@@ -78,6 +82,11 @@ class HomeFragment : Fragment() {
 //
 //        }
 
+=======
+        val hh = requireActivity().findViewById<TextView>(R.id.hh)
+        val response = sendGetRequest("127.0.0.1:8000:simple/")
+        hh.text = response
+>>>>>>> parent of df4633c (test1)
 
 
 
