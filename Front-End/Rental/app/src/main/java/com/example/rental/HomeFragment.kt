46b1a3d8e278376ba09c716966594a3bc8627986
jmamine <109294107+jmamine@users.noreply.github.com>
2023-02-22 11:20:28 +0100

    it_works

--- Front-End/Rental/app/src/main/java/com/example/rental/HomeFragment.kt
+++ Front-End/Rental/app/src/main/java/com/example/rental/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,28 +30,42 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getcar()
 
-        val vm = MarqueModel()
-        val view = requireActivity().findViewById<RecyclerView>(R.id.rvBrands)
-        val layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
-        view.layoutManager = layoutManager
-        view.adapter = MarqueAdapter(requireContext(), vm.data)
+      //  val vm = MarqueModel()
+      //  val view = requireActivity().findViewById<RecyclerView>(R.id.rvBrands)
+      //  val layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+      //  view.layoutManager = layoutManager
+      //  view.adapter = MarqueAdapter(requireContext(), vm.data)
 
-        val vmCars = CarModel()
-        val viewCars = requireActivity().findViewById<RecyclerView>(R.id.rvCars)
-        val layoutManagerCars = GridLayoutManager(requireContext(), 2)
-        viewCars.layoutManager = layoutManagerCars
-        viewCars.adapter = CarAdapter(requireContext(), vmCars.data)
-        val hh = requireActivity().findViewById<TextView>(R.id.hh)
+
+        //val hh = requireActivity().findViewById<TextView>(R.id.brand)
             //val response = sendGetRequest("http://localhost:8000/simple/")
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = sendGetRequest("http://127.0.0.1:9000/simple/")
+
+
+
+
+
+
+
+
+    }
+
+    private fun getcar(){
+
+        GlobalScope.launch(Dispatchers.IO) {GlobalScope.launch(Dispatchers.IO) {
+            val response = sendGetRequest("https://0382-105-235-130-147.eu.ngrok.io/all_objects/")
             withContext(Dispatchers.Main) {
                 // Update the UI with the result
-                hh.text = response
+
+                val viewCars = requireActivity().findViewById<RecyclerView>(R.id.rvCars)
+                val layoutManagerCars = GridLayoutManager(requireContext(), 2)
+                viewCars.layoutManager = layoutManagerCars
+                viewCars.adapter = CarAdapter(requireContext(), response as ArrayList<Car>)
             }
         }
+
 
 
 
@@ -59,4 +74,5 @@ class HomeFragment : Fragment() {
 
 
 
+}
 }
